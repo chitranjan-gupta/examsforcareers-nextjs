09f6a4e6233b458839db1d1d3501a1ec64c502c8
Chitranjan Kumar Gupta <37315561+chitranjan01@users.noreply.github.com>
2021-09-02 10:16:27 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    MONGO_URL: "mongodb://localhost/exams",
-    SECRET_KEY: "EXAMSFORCAREERSCOMPANY",
-    PORT: 5000,
-    ADMIN_SECRET_KEY: "ADMINEXAMSFORCAREERSCOMPANY",
-  },
-};
